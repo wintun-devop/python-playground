5da43e60c341f6c915a02c717c446406b54d93ac
Win Tun <wintun.hlaing@outlook.com>
2024-01-17 22:33:22 +0630

    create ok

---
+++ orm-sqlalchemy-2.0/crud.py
@@ -0,0 +1,17 @@
+from models.db import db_session
+from models import Product
+from sqlalchemy.exc import SQLAlchemyError
+import uuid
+
+def create():
+    add_person = Product(name='Mikrotik L009UiGS-RM',model_no='L009UiGS-RM-T1',description='L009 is more than just a router.')
+    try:
+        db_session.add(add_person)
+        db_session.commit()
+        result = db_session.query(Product)
+        print({"msg":"success"})
+    except SQLAlchemyError as e:
+        print(e)
+
+create()
+    
---
+++ orm-sqlalchemy-2.0/models/__init__.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column,sessionmaker, scoped_session
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import func
+
+class Base(DeclarativeBase):
+    pass
+
+class Product(Base):
+    __tablename__ = "product"
+    id:Mapped[str] = mapped_column(UUID(as_uuid=True), primary_key=True, default=func.uuid_generate_v4())
+    name:Mapped[str] = mapped_column(nullable=False)
+    model_no:Mapped[str] = mapped_column(nullable=False, unique=True)
+    description:Mapped[str] = mapped_column(nullable=False)
+    
+
+    
+
+    
