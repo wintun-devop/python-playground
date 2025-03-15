b41636a9c5ad53b3ca54178a1a12d31fa8dab783
wintun-devop <wintun.hlaing@outlook.com>
2025-03-15 23:34:41 +0630

    csv-export-complete

--- csv-operation/mysql-csv-export/csv_export_origin_csv_mysql.py
+++ csv-operation/mysql-csv-export/csv_export_origin_csv_mysql.py
@@ -24,8 +24,22 @@ def connection(db_host,db_user,db_password,db_name):
 db_connection = connection(db_host,db_user,db_password,db_name)
 
 def mysql_to_csv():
+    sql_file = r"D:\python-servers\python-playground\csv-operation\mysql-csv-export\sample_sql_script.sql"
+    with open(sql_file) as f:
+        sql_queries = f.read()
+    print(f"{date_time}>>>sql read data",sql_queries)
     cursor = db_connection.cursor()
     print(f"{date_time}>>db_cursor",cursor)
+    cursor.execute(sql_queries)
+    data = cursor.fetchall()
+    print(f"{date_time}>>rows",data)
+    columns = [desc[0] for desc in cursor.description]  # Column names
+    print(f"{date_time}>>columl",columns)
+    with open(f"output_origin_package.csv", "w", newline="", encoding="utf-8") as csvfile:
+        fields = columns
+        csv_writer = csv.DictWriter(csvfile,fieldnames=fields,quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writeheader()
+        csv_writer.writerows(data)
     print(f"{date_time}>>Data has been exported to output.csv")
     result = {"status":"success"}
     return result
--- docs/mysql-database-table-create.md
+++ docs/mysql-database-table-create.md
@@ -31,11 +31,18 @@ name text NOT NULL,
 model_no varchar(255) NOT NULL UNIQUE,
 description text NOT NULL,
 arrival varchar(255) NOT NULL,
+qty INT NOT NULL,
 created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY(id)
 );
 ```
 ### Insert Testing
 ```
-INSERT INTO product (id,name,model_no,description,arrival) VALUES (uuidv4(),'Mikrotik L009UiGS-RM','L009UiGS-RM','L009 is more than just a router.','1/2/2012');
+INSERT INTO products (id,name,model_no,description,arrival,qty) VALUES ('uiefaf-fusfklff','Mikrotik L009UiGS-RM','L009UiGS-RM','L009 is more than just a router.','2025-03-15',15);
+```
+```
+INSERT INTO products (id,name,model_no,description,arrival,qty) VALUES ('uiafef-fusfklff','Mikrotik L011UiGS-RM','L011UiGS-RM','L011 is more than just a router.','2025-03-15',30);
+```
+```
+INSERT INTO products (id,name,model_no,description,arrival,qty) VALUES ('uiafef-fusfilgg','Mikrotik L012UiGS-RM','L012UiGS-RM','L012UiGS-RMはrouterより良いです。','2025-03-15',22);
 ```
